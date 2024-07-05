2a7c4212346f1888b8b134a76cb9530b9232ab12
Kyle Martin <kyle.martin@middleware.co.nz>
2024-07-05 14:53:15 +1200

    Add shebang to release file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   target: 'node',
@@ -7,4 +8,10 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'cli.js',
   },
+  plugins: [
+    new webpack.BannerPlugin({
+      raw: true,
+      banner: '#!/usr/bin/env node',
+    }),
+  ]
 };
